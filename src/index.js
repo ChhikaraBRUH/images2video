7d96541e898880cc3113d55779750caa3efd4dfd
ChhikaraBRUH <chaitanyachhikara@gmail.com>
2023-12-27 18:34:47 +0530

    fix: add port env

--- src/index.js
+++ src/index.js
@@ -7,13 +7,17 @@ import downloadImage from "./downloadImage.js";
 
 ffmpeg.setFfmpegPath(ffmpegInstaller.path);
 ffmpeg.setFfprobePath(ffprobeInstaller.path);
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.status(200).send("iamges2video express server is running");
+});
+
+app.get("/hello", (req, res) => {
+  res.status(200).send("Hi, it's all godd here");
 });
 
 app.post("/video", async (req, res) => {
